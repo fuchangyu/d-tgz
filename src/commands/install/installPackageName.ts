8ccdef0b5b45afd69d02cc5d8ab9e424d8505a18
fuchangyu <1334765151@qq.com>
2024-12-19 11:22:31 +0800

    i18n

--- src/commands/install/installPackageName.ts
+++ src/commands/install/installPackageName.ts
@@ -3,6 +3,7 @@ import { TEMP_PACKAGE_LOCK_PATH, TEMP_PACKAGE_PATH, TEMP_PATH } from "../../glob
 import { downloadPackages, parseLock, readLock, spinner } from "../../utils/utils";
 import cmd from "node-cmd";
 import { LockData } from "../../types";
+import { i18n } from '../../i18n';
 
 
 const basePackageContent = {
@@ -20,7 +21,7 @@ export async function installPackageName (name: string) {
 
   fs.writeFileSync(TEMP_PACKAGE_PATH, JSON.stringify(basePackageContent))
 
-  spinner.start(`正在解析${ name }的依赖`)
+  spinner.start(i18n.__('parsingPackage') + name)
 
 
   const run = cmd.runSync('cd ' + TEMP_PATH + '&&' + `npm install ${ name } --package-lock-only`)
@@ -28,7 +29,7 @@ export async function installPackageName (name: string) {
   if (run.err) {
     fs.removeSync(TEMP_PATH)
     spinner.stop()
-    spinner.fail(`解析${ name }的依赖失败！`)
+    spinner.fail(i18n.__('parsePackageFailed') + name)
     process.exit(0)
   }
 
